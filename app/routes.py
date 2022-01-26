7340045a3e2b11938b5482ae0335ebc7de970caa
VebjornRiiser <12122688+VebjornRiiser@users.noreply.github.com>
2022-01-26 15:07:59 +0100

    Removed duplicate routes and added gitignore

--- app/routes.py
+++ app/routes.py
@@ -33,31 +33,6 @@ def update_data():
         app.config["controller_data"] = json.loads(request.data)
         # print("after")
     return "good"
-
-
-@app.route('/test')
-def test():
-    if not "controller_data" in app.config:
-        app.config["controller_data"] = "a"
-    return json.dumps(app.config["controller_data"])
-
-@app.route('/view')
-def view():
-    return render_template("view.html")
-
-
-
-@app.route('/update_data', methods=["POST"])
-def update_data():
-    if request.method == 'POST':
-        # print("before")
-        # print(jsonstr(request.data))
-        # print(json.loads(request.data)[0])
-        app.config["controller_data"] = json.loads(request.data)
-        # print("after")
-    return "good"
-
-
 # -------------------------------------------------------------
 # JSON-filer til kommunikasjon mellom ROV og brukergrensesnitt
 
